7158022981e98518c9b9a87e8ae79e6daf8ad2e9
Spica-blue <heo1356@gmail.com>
2024-09-09 15:17:25 +0900

    메인 css 완료

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ import ManagementLayout from "./pages/Management/ManagementLayout";
 import PrivateRouteAdmin from "./services/Management/PrivateRouteAdmin"
 import PrivateRoute  from "./services/PrivateRoute";
 import Gallery from "./pages/Gallery/Gallery.jsx";
-import Mainc from "./pages/Main/Main copy.jsx";
 
 // import index from '../src/styles/index.css'
 
@@ -58,7 +57,7 @@ function App() {
           <Route path="/SignIn" element={<SignIn />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/Loading" element={<Loading />} />
-          <Route path="/Main" element={<PrivateRoute component={<Mainc />} />} />
+          <Route path="/Main" element={<PrivateRoute component={<Main />} />} />
           <Route path="/Promotion" element={<Promotion />} />
           <Route path="/Logout" element={<Logout/>}/>
           <Route path="/AiSearch" element={<PrivateRoute component={<AiSearch />}/>} />
